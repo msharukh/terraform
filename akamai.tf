4266153376a349118f6d69a45ab1a80dfa61abf6
Mohammed Sharukh <msharukh853@gmail.com>
2024-08-08 20:16:15 +0000

    added edgerc

--- akamai.tf
+++ akamai.tf
@@ -2,3 +2,11 @@ provider "akamai" {
   edgerc         = "~/.edgerc"
   config_section = "default"
 }
+
+provider "akamai" {
+  client_secret = "OViP7OTbR8riYvNKe/WSLSg2E0AKDiOKij0ZIOxtcDw="
+  host          = "akab-sppzpgi6d7hfjfty-mp6sb33rc2mhxbkw.luna.akamaiapis.net"
+  access_token  = "akab-wkhwc333x526x7el-rrl34gg3hrykep55"
+  client_token  = "akab-vcdx7xfwm6wn3msp-hyau67f6nuoyujfi"
+  config_section = "default"
+}
