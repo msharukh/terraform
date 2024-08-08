a1167e6fff42c1f25143790d7a3422036663f5a0
Mohammed Sharukh <msharukh853@gmail.com>
2024-08-08 20:31:59 +0000

    test

--- akamai.tf
+++ akamai.tf
@@ -10,3 +10,35 @@ provider "akamai" {
   client_token  = "akab-vcdx7xfwm6wn3msp-hyau67f6nuoyujfi"
   config_section = "default"
 }
+git clone https://github.com/hashicorp/terraform.git
+
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    akamai = {
+      source  = "akamai/akamai"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    akamai = {
+      source  = "akamai/akamai"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "akamai" {}
+
+data "akamai_property_rules" "rules" {
+  property_id = "prp_1"
+}
+
+output "json" {
+  value = data.akamai_property_rules.rules
+}_1"
